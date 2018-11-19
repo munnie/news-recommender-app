405165c94429fbfcf35d2cf0d75c6df893480a7c
munnie <nthnam91@gmail.com>
2018-11-19 21:07:34 +0200

    Initial Commit

---
+++ NewsRecommender/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NewsRecommender
+//
+//  Created by mun on 11/19/18.
+//  Copyright © 2018 mun. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
